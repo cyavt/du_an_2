115d0112d0d59f7862cf71e313233aa507934545
Nguyễn Văn Trúc <vantruc572003@gmail.com>
2023-03-06 10:53:57 +0700

    update 06/03/22

--- meta/footer.php
+++ meta/footer.php
@@ -100,7 +100,120 @@
 <script src="assets/js/util.js"></script>
 <script src="assets/js/main.js"></script>
 <script>
-    var _0xf9f5=["\x64\x65\x74\x61\x69\x6C","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x6D\x79\x4D\x6F\x64\x61\x6C","\x6D\x79\x42\x74\x6E","\x63\x6C\x6F\x73\x65","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x43\x6C\x61\x73\x73\x4E\x61\x6D\x65","\x2E\x64\x65\x76","\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72\x41\x6C\x6C","\x63\x6C\x69\x63\x6B","\x64\x69\x73\x70\x6C\x61\x79","\x73\x74\x79\x6C\x65","\x62\x6C\x6F\x63\x6B","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x6E\x6F\x6E\x65","\x74\x61\x72\x67\x65\x74","\x6C\x65\x6E\x67\x74\x68","\x53\x6F\x72\x72\x79\x20\x3A\x28","\x43\x68\u1EC9\x20\x63\xF3\x20\x74\x68\xE0\x6E\x68\x20\x76\x69\xEA\x6E\x20\x6D\u1EDB\x69\x20\x73\u1EED\x20\x64\u1EE5\x6E\x67\x20\u0111\u01B0\u1EE3\x63\x20\x74\xED\x6E\x68\x20\x6E\u0103\x6E\x67\x20\x6E\xE0\x79\x20\x21","\x69\x6E\x66\x6F","\x6D\x79\x53\x6C\x69\x64\x65\x73\x33","\x69\x6D\x61\x67\x65\x2D\x62\x61\x64\x67\x65","\x63\x6C\x61\x73\x73\x4E\x61\x6D\x65","\x20\x62\x61\x64\x67\x65\x2D\x77\x68\x69\x74\x65","\x20","\x72\x65\x70\x6C\x61\x63\x65","","\x73\x75\x62\x6D\x69\x74","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x73\x63\x72\x69\x70\x74\x2E\x67\x6F\x6F\x67\x6C\x65\x2E\x63\x6F\x6D\x2F\x6D\x61\x63\x72\x6F\x73\x2F\x73\x2F\x41\x4B\x66\x79\x63\x62\x79\x74\x71\x66\x59\x6A\x73\x66\x6E\x67\x66\x55\x6A\x64\x46\x6D\x77\x4D\x62\x67\x42\x57\x32\x68\x6C\x38\x30\x4B\x33\x6C\x4B\x66\x47\x33\x4C\x75\x35\x70\x57\x53\x75\x34\x6A\x6A\x74\x6B\x30\x73\x51\x58\x66\x57\x76\x71\x6F\x6B\x39\x63\x34\x54\x69\x6C\x4E\x30\x44\x32\x2F\x65\x78\x65\x63","\x47\x45\x54","\x73\x65\x72\x69\x61\x6C\x69\x7A\x65","\x23\x41\x64\x64","\x64\x69\x73\x61\x62\x6C\x65\x64","\x70\x72\x6F\x70","\u0110\x61\x6E\x67\x20\x78\u1EED\x20\x6C\xFD\x2E\x2E\x2E","\x74\x65\x78\x74","\x23\x73\x62\x74\x6E","\x6C\x6F\x67","\x47\u1EED\x69","\x72\x65\x73\x75\x6C\x74","\x73\x75\x63\x63\x65\x73\x73","\x72\x65\x6C\x6F\x61\x64","\x74\x68\x65\x6E","\x54\x68\xE0\x6E\x68\x20\x63\xF4\x6E\x67\x20\x21","\x54\x68\xEA\x6D\x20\x74\x68\xE0\x6E\x68\x20\x63\xF4\x6E\x67","\x6E\x75\x6C\x6C","\x4C\u1ED7\x69\x20\x21","\x56\x75\x69\x20\x6C\xF2\x6E\x67\x20\u0111\x69\u1EC1\x6E\x20\u0111\u1EE7\x20\x74\x68\xF4\x6E\x67\x20\x74\x69\x6E\x21","\x65\x72\x72\x6F\x72","\x56\x75\x69\x20\x6C\xF2\x6E\x67\x20\x74\x68\u1EED\x20\x6C\u1EA1\x69\x20\x21","\u0110\xE3\x20\x78\u1EA3\x79\x20\x72\x61\x20\x6C\u1ED7\x69\x21","\u0110\xE3\x20\x78\u1EA3\x79\x20\x72\x61\x20\x6C\u1ED7\x69\x20\x63\u1EE5\x63\x20\x62\u1ED9\x2C\x20\x76\x75\x69\x20\x6C\xF2\x6E\x67\x20\x74\x68\u1EED\x20\x6C\u1EA1\x69\x21","\x61\x6A\x61\x78","\x6F\x6E"];const home=document[_0xf9f5[1]](_0xf9f5[0]);const modal=document[_0xf9f5[1]](_0xf9f5[2]);const btn=document[_0xf9f5[1]](_0xf9f5[3]);const span=document[_0xf9f5[5]](_0xf9f5[4])[0];const dev=document[_0xf9f5[7]](_0xf9f5[6]);btn[_0xf9f5[12]](_0xf9f5[8],function(){modal[_0xf9f5[10]][_0xf9f5[9]]= _0xf9f5[11]});span[_0xf9f5[12]](_0xf9f5[8],function(){modal[_0xf9f5[10]][_0xf9f5[9]]= _0xf9f5[13]});window[_0xf9f5[12]](_0xf9f5[8],function(){if(event[_0xf9f5[14]]== modal){modal[_0xf9f5[10]][_0xf9f5[9]]= _0xf9f5[13]}});for(let i=0;i< dev[_0xf9f5[15]];i++){dev[i][_0xf9f5[12]](_0xf9f5[8],function(){swal(_0xf9f5[16],_0xf9f5[17],_0xf9f5[18])})};if(home!= undefined){var slideIndex=1;showDivs(slideIndex);carousel2();function plusDivs(_0x5f67x9){showDivs(slideIndex+= _0x5f67x9)}function currentDiv(_0x5f67x9){showDivs(slideIndex= _0x5f67x9)}function showDivs(_0x5f67x9){const _0x5f67xc=document[_0xf9f5[5]](_0xf9f5[19]);const _0x5f67xd=document[_0xf9f5[5]](_0xf9f5[20]);let i;if(_0x5f67x9> _0x5f67xc[_0xf9f5[15]]){slideIndex= 1};if(_0x5f67x9< 1){slideIndex= _0x5f67xc[_0xf9f5[15]]};for(i= 0;i< _0x5f67xc[_0xf9f5[15]];i++){_0x5f67xc[i][_0xf9f5[10]][_0xf9f5[9]]= _0xf9f5[13]};for(i= 0;i< _0x5f67xd[_0xf9f5[15]];i++){_0x5f67xd[i][_0xf9f5[21]]= _0x5f67xd[i][_0xf9f5[21]][_0xf9f5[24]](_0xf9f5[22],_0xf9f5[23])};_0x5f67xc[slideIndex- 1][_0xf9f5[10]][_0xf9f5[9]]= _0xf9f5[11];_0x5f67xd[slideIndex- 1][_0xf9f5[21]]+= _0xf9f5[22]}function carousel2(){let i;const _0x5f67xc=document[_0xf9f5[5]](_0xf9f5[19]);const _0x5f67xd=document[_0xf9f5[5]](_0xf9f5[20]);for(i= 0;i< _0x5f67xc[_0xf9f5[15]];i++){_0x5f67xc[i][_0xf9f5[10]][_0xf9f5[9]]= _0xf9f5[13]};if(slideIndex> _0x5f67xc[_0xf9f5[15]]){slideIndex= 1};for(i= 0;i< _0x5f67xd[_0xf9f5[15]];i++){_0x5f67xd[i][_0xf9f5[21]]= _0x5f67xd[i][_0xf9f5[21]][_0xf9f5[24]](_0xf9f5[22],_0xf9f5[25])};_0x5f67xc[slideIndex- 1][_0xf9f5[10]][_0xf9f5[9]]= _0xf9f5[11];_0x5f67xd[slideIndex- 1][_0xf9f5[21]]+= _0xf9f5[22];setTimeout(carousel2,2000);slideIndex++}};$(_0xf9f5[31])[_0xf9f5[53]](_0xf9f5[26],(function(_0x5f67xf){_0x5f67xf[_0xf9f5[27]]();$[_0xf9f5[52]]({url:_0xf9f5[28],type:_0xf9f5[29],data:$(_0xf9f5[31])[_0xf9f5[30]](),contentType:false,cache:false,processData:false,beforeSend:function(){$(_0xf9f5[36])[_0xf9f5[35]](_0xf9f5[34])[_0xf9f5[33]](_0xf9f5[32],true)},success:function(_0x5f67x10){console[_0xf9f5[37]](_0x5f67x10);$(_0xf9f5[36])[_0xf9f5[35]](_0xf9f5[38])[_0xf9f5[33]](_0xf9f5[32],false);if(_0x5f67x10[_0xf9f5[39]]== _0xf9f5[40]){swal(_0xf9f5[43],_0xf9f5[44],_0xf9f5[40])[_0xf9f5[42]](function(){location[_0xf9f5[41]]()})}else {if(_0x5f67x10== _0xf9f5[45]){swal(_0xf9f5[46],_0xf9f5[47],_0xf9f5[48])}else {swal(_0xf9f5[46],_0xf9f5[49],_0xf9f5[48])}}},error:function(){swal(_0xf9f5[50],_0xf9f5[51],_0xf9f5[48]);$(_0xf9f5[36])[_0xf9f5[35]](_0xf9f5[38])[_0xf9f5[33]](_0xf9f5[32],false)}})}))
+    const home = document.getElementById('detail');
+
+    const modal = document.getElementById('myModal');
+    const btn = document.getElementById("myBtn");
+    const span = document.getElementsByClassName("close")[0];
+    const dev = document.querySelectorAll('.dev');
+
+    btn.addEventListener('click', function() {
+        modal.style.display = "block";
+    })
+    span.addEventListener('click', function() {
+        modal.style.display = "none";
+    })
+    window.addEventListener('click', function() {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    })
+
+    for (let i = 0; i < dev.length; i++) {
+        dev[i].addEventListener('click', function() {
+            swal(
+                'Sorry :(',
+                'Chỉ có thành viên mới sử dụng được tính năng này !',
+                'info'
+            )
+        });
+    }
+    /* slide */
+    if (home != undefined) {
+        var slideIndex = 1;
+        showDivs(slideIndex);
+        carousel2();
+
+        function plusDivs(n) {
+            showDivs(slideIndex += n);
+        }
+
+        function currentDiv(n) {
+            showDivs(slideIndex = n);
+        }
+
+        function showDivs(n) {
+            const x = document.getElementsByClassName("mySlides3");
+            const dots = document.getElementsByClassName("image-badge");
+            let i;
+            if (n > x.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = x.length
+            }
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" badge-white", " ");
+            }
+            x[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " badge-white";
+            
+        }
+
+        function carousel2() {
+            let i;
+            const x = document.getElementsByClassName("mySlides3");
+            const dots = document.getElementsByClassName("image-badge");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            if (slideIndex > x.length) {
+                slideIndex = 1
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" badge-white", "");
+            }
+            x[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " badge-white";
+            setTimeout(carousel2, 2000);
+            slideIndex++;
+        }
+    }
+    /* <!-- POST data --> */
+    $("#Add").on('submit', (function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbytqfYjsfngfUjdFmwMbgBW2hl80K3lKfG3Lu5pWSu4jjtk0sQXfWvqok9c4TilN0D2/exec",
+            type: "GET",
+            data: $("#Add").serialize(),
+            contentType: false,
+            cache: false,
+            processData: false,
+            beforeSend: function() {
+                $("#sbtn").text('Đang xử lý...').prop('disabled', true)
+            },
+            success: function(data) {
+                console.log(data)
+                $("#sbtn").text('Gửi').prop('disabled', false)
+                if (data['result'] == 'success')
+                    swal("Thành công !", "Thêm thành công", "success").then(function() {
+                        location.reload();
+                    })
+                else if (data == 'null')
+                    swal("Lỗi !", "Vui lòng điền đủ thông tin!", "error")
+                else
+                    swal("Lỗi !", "Vui lòng thử lại !", "error")
+            },
+            error: function() {
+                swal("Đã xảy ra lỗi!", "Đã xảy ra lỗi cục bộ, vui lòng thử lại!", "error")
+                $("#sbtn").text('Gửi').prop('disabled', false)
+            }
+        });
+    }))
 </script>
 </body>
 
