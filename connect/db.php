7e072ad10a54c697fb8f80719acd620ad387cbd3
Nguyễn Văn Trúc <vantruc572003@gmail.com>
2023-11-04 00:53:34 +0700

    UPLOAD END 1

--- connect/db.php
+++ connect/db.php
@@ -1,12 +1,26 @@
 <?php
+$env = file_get_contents('.env');
+$env = explode("\n", $env);
+
+$envVars = [];
+foreach ($env as $line) {
+    $line = trim($line);
+    if ($line !== '' && $line[0] !== '#') {
+        $parts = explode('=', $line, 2);
+        if (count($parts) === 2) {
+            list($key, $value) = $parts;
+            $envVars[$key] = $value;
+        } else {
+            // Xử lý khi dòng không hợp lệ
+            echo "Dòng không hợp lệ: $line\n";
+        }
+    }
+}
+
 /* DATABASE */
 define('HACKER', 'Lỗi không thể truy cập đường dẫn này !');
 define('ADMIN', 'admin');
-define('SERVERNAME', 'localhost');
-define('USERNAME', 'root');
-define('DATABASE', 'du_an_2023');
-define('PASSWORD', '');
-$connect = new mysqli(SERVERNAME, USERNAME, PASSWORD, DATABASE);
+$connect = new mysqli($envVars['DB_HOST'], $envVars['DB_USER'], $envVars['DB_PASSWORD'], $envVars['DB_DB']);
 if ($connect->connect_error) {
     die("Không thể kết nối");
     exit();
